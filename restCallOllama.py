a0d55b89fbb32d5f0e08a8c9bd96963dd9ee3230
christon james navaraj <christonnavasalo@gmail.com>
2024-10-16 20:23:40 +0530

    Commit for using ollama running on local for chat testing restcalls

--- restCallOllama.py
+++ restCallOllama.py
@@ -6,7 +6,8 @@ def getprompt(cookievalue, modelname, query):
 
     payload = json.dumps({
     "model": modelname,
-    "query": query
+    "query": query,
+    "limit" : 250
     })
     headers = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
