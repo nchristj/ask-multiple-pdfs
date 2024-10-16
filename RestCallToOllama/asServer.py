7886d835369851c99386913a51f4fa2817c81704
christon james navaraj <christonnavasalo@gmail.com>
2024-10-16 20:20:55 +0530

    Commit for using ollama running on local for chat testing restcalls

--- RestCallToOllama/asServer.py
+++ RestCallToOllama/asServer.py
@@ -4,6 +4,9 @@ from query_data import query_rag
 from langchain_community.llms.ollama import Ollama
 from langchain_community.vectorstores import Chroma
 from get_embedding_function import get_embedding_function
+from langchain_ollama import OllamaEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.prompts import ChatPromptTemplate
 
 CHROMA_PATH = "mistraldb"
 embedding_function = get_embedding_function()
@@ -18,6 +21,34 @@ app = Flask(__name__)
 @app.route('/appinfo', methods=['GET'])
 def appinfo():
     return "Hello World! Im still Alive Please reach to Christon James if you not see this message!!!"
+
+@app.route('/getprompt', methods=['GET'])
+def getprompt():
+    input_query = request.get_json()
+    print(input_query)
+    question = input_query.get('query')
+    limit = input_query.get('limit')
+
+    #embeddings = OllamaEmbeddings(model="mistral")
+    #print(type(embeddings))
+    #vectorstore_ = FAISS.load_local("FAISS_", embedding_function, "index", allow_dangerous_deserialization=True)
+    results = db.similarity_search_with_score(question, k=4)
+    context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+    if limit != None and int(limit) < len(context_text):
+        context_text = context_text[0:limit]
+    PROMPT_TEMPLATE = """
+    Answer the question based only on the following context:
+
+    {context}
+
+    ---
+
+    Answer the question based on the above context: {question}
+    Please provide answer in JSON format with tags : question, answer.
+    """
+    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+    prompt = prompt_template.format(context=context_text, question=question)
+    return (prompt)
 
 @app.route('/queryrag', methods=['POST'])
 def hello():
--- restCallOllama.py
+++ restCallOllama.py
@@ -6,8 +6,7 @@ def getprompt(cookievalue, modelname, query):
 
     payload = json.dumps({
     "model": modelname,
-    "query": query,
-    "limit" : 250
+    "query": query
     })
     headers = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
@@ -62,11 +61,9 @@ def getresponse(cookievalue, modelname, prompt):
     return response
 
 modelname = "mistral"
-question = 'Define WAARR'
 cookievalue = 'ajs_user_id=5f8e64255d4b1b169dda6eb7ccb8ba879c593fe0; ajs_anonymous_id=09c0e1a7-8d45-4024-946c-ad0f7f3a9340; _oauth_proxy=bmNocmlzdGpAY2x1c3Rlci5sb2NhbA==|1729044885|TjsXsLsySIP2uAAiuaxRK41G0hM=; _streamlit_xsrf=2|beab9f4f|58e663cf0be897a29b016e04cdfaa521|1729045055; analytics_session_id=1729077339662; analytics_session_id.last_access=1729077353792; 0a29b96fbcf1b5b4496eec99edbd0f75=65d59b6452a90b54a1de4b050ce9174e'
 
-def getResponse():
+def getrestcallsdone(question):
     prompt = getprompt(cookievalue,modelname, question)
     response = getresponse(cookievalue, modelname, prompt)
     return response
-print(getResponse())
